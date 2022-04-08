4a875fba928703c36fd1dbae1efc3780a7765831
Miller <milleri@wwt.com>
2022-04-08 10:03:24 -0500

    component organization

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,5 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
+import React from "react";
+
 function App() {
     const expenses = [
         {
@@ -22,6 +23,13 @@ function App() {
             date: new Date(2021, 5, 12),
         },
     ];
+
+    // return React.createElement(
+    //     'div',
+    //     {},
+    //     React.createElement('h2', {}, "let's get started!"),
+    //     React.createElement(Expenses, {items: expenses})
+    // )
 
     return (
         <div>
